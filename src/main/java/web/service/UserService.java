d3a4507be882fb833d1c47fba2e15d65efaeaab5
imedfan <54060074+imedfan@users.noreply.github.com>
2022-06-23 13:13:16 +0200

    finalDao

--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -1,35 +1,12 @@
 package web.service;
-
-import org.springframework.stereotype.Component;
-
+import web.model.*;
 import java.util.List;
 
-import web.model.User;
 
-@Component
-public class UserService {
-    private List<User> usersList;
-
-    public UserService(List<User> usersList) {
-        this.usersList = usersList;
-
-        User user1 = new User("Artem",27, "Volvo", true);
-        User user2 = new User("Petr",15, "toyCars", false);
-        User user3 = new User("John", 54, "Mersedes", false);
-        User user4 = new User("Irina", 37, "Lava", true);
-
-        usersList.add(user1);
-        usersList.add(user2);
-        usersList.add(user3);
-        usersList.add(user4);
-    }
-
-    public List<User> getUsersList(int i) {
-        if (i > 0 && i < usersList.size()) {
-            return usersList.subList(0, i);
-        }
-        return usersList;
-    }
-
-
+public interface UserService {
+    void addUser(User user);
+    User getUser(long id);
+    void deleteUser(int id);
+    void updateUser(User user);
+    List<User> getAllUsers();
 }
